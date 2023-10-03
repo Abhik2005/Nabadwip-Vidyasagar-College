737151d3106bbb463c6b75280222e720a5656a23
Abhik532 <abhikmondal532@gmail.com>
2023-10-03 14:08:01 +0530

    complete

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ Time_DateFunction();
 function OpacityAnimation() {
     setInterval(function () {
       gsap.to("#clp", {
-        opacity: 0.2,
-        duration: 0.2,
+        opacity: 0.1,
+        duration: 0.1,
       });
       gsap.to("#clp", {
         opacity: 1,
-        delay: 0.3,
-        duration: 0.2,
+        delay: 0.2,
+        duration: 0.5,
       });
     }, 800);
 }
